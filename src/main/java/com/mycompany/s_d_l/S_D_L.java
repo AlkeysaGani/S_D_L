e87005a1fc45a373e3bb2c7e61b24325214afb05
hp <hp@LAPTOP-5FRCRN9S>
2026-02-13 17:14:23 +0700

    Test

---
+++ src/main/java/com/mycompany/s_d_l/S_D_L.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.s_d_l;
+
+/**
+ *
+ * @author hp
+ */
+public class S_D_L {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
